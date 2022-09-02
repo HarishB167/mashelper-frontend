69d506ae23bd54a82af15fffae3e6d27b326c3dc
Harish <harishpoctor@gmail.com>
2022-09-02 22:12:55 +0530

    First draft of Home page completed.

---
+++ src/components/common/navBar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+function NavBar(props) {
+  return (
+    <nav class="navbar navbar-expand-lg bg-light navbar__gradient_blue">
+      <div class="container-fluid">
+        <a class="navbar-brand navbar-brand__color_white" href="#">
+          MAS Helper
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a
+                class="nav-link active nav-link__color_white"
+                aria-current="page"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link nav-link__color_white" href="#">
+                About
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link nav-link__color_white" href="#">
+                Help
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ src/components/home.jsx
@@ -0,0 +1,199 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <div className="container container-sm">
+      <div>
+        <a className="btn btn-primary btn-sm m-2" href="">
+          Download Data in CSV
+        </a>
+        <a className="btn btn-primary btn-sm m-2" href="">
+          View Data
+        </a>
+      </div>
+      <form>
+        <label className="form-label mt-2 mb-0" htmlFor="date">
+          Date of Issue/Consumption
+        </label>
+        <input className="form-control" name="date" type="date" required />
+        <label className="form-label mt-2 mb-0" htmlFor="location">
+          Location
+        </label>
+        <input className="form-control" name="location" type="text" required />
+        <label className="form-label" mt-2 mb-0 htmlFor="remarks">
+          Remarks
+        </label>
+        <input className="form-control" name="remarks" type="text" required />
+        <div className="row">
+          <fieldset className="col col-6">
+            <legend>Add Item</legend>
+            <label className="form-label mt-2 mb-0" htmlFor="material">
+              Material Name
+            </label>
+            <input
+              className="form-control"
+              name="material"
+              type="text"
+              required
+            />
+            <label className="form-label mt-2 mb-0" htmlFor="quantity">
+              Quantity
+            </label>
+            <input
+              className="form-control"
+              name="quantity"
+              type="number"
+              min="0"
+              required
+            />
+            <label className="form-label mt-2 mb-0" htmlFor="unit">
+              Unit
+            </label>
+            <select className="form-control" name="unit" id="unit" required>
+              <option value="">Select unit...</option>
+              <option value="EA">EA</option>
+              <option value="M">M</option>
+              <option value="KG">KG</option>
+              <option value="ROL">ROL</option>
+              <option value="L">L</option>
+              <option value="ST">ST</option>
+            </select>
+            <button className="btn btn-primary btn-sm m-2" type="submit">
+              Add Item
+            </button>
+            <button className="btn btn-primary btn-sm m-2" type="submit">
+              Save
+            </button>
+          </fieldset>
+          <div className="col col-6">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Material</th>
+                  <th>Quantity</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody className="table__tbody">
+                <tr>
+                  <td>Eyehook</td>
+                  <td>4 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Trf.Oil</td>
+                  <td>10 L</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>IPC P4</td>
+                  <td>10 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>LT Tape</td>
+                  <td>2 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Eyehook</td>
+                  <td>4 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Trf.Oil</td>
+                  <td>10 L</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>IPC P4</td>
+                  <td>10 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>LT Tape</td>
+                  <td>2 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Eyehook</td>
+                  <td>4 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Trf.Oil</td>
+                  <td>10 L</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>IPC P4</td>
+                  <td>10 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>LT Tape</td>
+                  <td>2 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Eyehook</td>
+                  <td>4 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Trf.Oil</td>
+                  <td>10 L</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>IPC P4</td>
+                  <td>10 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>LT Tape</td>
+                  <td>2 EA</td>
+                  <td>
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Home;
